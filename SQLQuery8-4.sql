c0ce6389e12caf551f948931ff5fec39982abe55
timmoo <47295295+timmoo@users.noreply.github.com>
2019-04-11 11:33:14 +0300

    Update SQLQuery8-4.sql

--- SQLQuery8-4.sql
+++ SQLQuery8-4.sql
@@ -1,15 +1,16 @@
 SELECT 
-C.CustomerID, 
 C.CustomerName, 
 O.*
 FROM Sales.Customers AS C
 CROSS APPLY (SELECT 
-			DISTINCT TOP 2 IL1.UnitPrice AS [UnitPrice],  
+			DISTINCT TOP 2 I.CustomerID, IL1.UnitPrice AS [UnitPrice],  
 			IL1.StockItemID AS StockID, 
-			I.InvoiceDate AS [DATE]
+			I.InvoiceDate
 			FROM Sales.InvoiceLines  AS IL1
 			INNER JOIN Sales.Invoices AS I ON IL1.InvoiceID=I.InvoiceID
-			GROUP BY IL1.UnitPrice , IL1.StockItemID, I.InvoiceDate
-			ORDER BY IL1.UnitPrice DESC
+			WHERE I.CustomerID = C.CustomerID
+			GROUP BY IL1.UnitPrice , IL1.StockItemID, I.InvoiceDate, I.CustomerID
+			ORDER BY  I.CustomerID, IL1.UnitPrice DESC, I.InvoiceDate
+		
                ) AS O
-ORDER BY C.CustomerID;
+ORDER BY  O.CustomerID, O.UnitPrice DESC, O.InvoiceDate;
