44720b6019f809b4bcf6f792059188c451a78192
timmoo <47295295+timmoo@users.noreply.github.com>
2019-04-11 08:08:32 +0300

    Update SQLQuery8-1.sql

--- SQLQuery8-1.sql
+++ SQLQuery8-1.sql
@@ -1,16 +1,24 @@
-SELECT * FROM 
-	(
-		SELECT DISTINCT
-		SUBSTRING(CONVERT(varchar(10),I.InvoiceDate,104),4,8) AS [Date],
-		SUBSTRING(C.CustomerName,14,25) AS [ShortName], 
+SELECT 
+CONVERT(varchar(10),SalesOrderMonth,104) AS [Date], 
+[Peeples Valley, AZ],
+[Medicine Lodge, KS],
+[Gasport, NY], 
+[Sylvanite, MT],
+[Jessie, ND] 
+FROM 
+		(SELECT DISTINCT 
+		CA.SalesOrderMonth,
+		CustomerName.SpecName,
 		COUNT(I.InvoiceID) OVER (PARTITION BY  C.CustomerName, YEAR(I.InvoiceDate), MONTH(I.InvoiceDate))	AS [SalesCount] 
 		FROM Sales.Customers AS C
 		JOIN Sales.Invoices AS I
 		ON C.CustomerID=I.CustomerID
-		WHERE C.CustomerID>=2 AND C.CustomerID<=6 
-		) AS Sales
-PIVOT (SUM(SalesCount)
-FOR ShortName IN ([ (Peeples Valley, AZ)],[ (Medicine Lodge, KS)],[ (Gasport, NY)], [ (Sylvanite, MT)],[ (Jessie, ND)]))
+		CROSS APPLY (SELECT CAST(DATEADD(mm,DATEDIFF(mm,0,I.InvoiceDate),0) AS DATE) AS SalesOrderMonth) AS CA
+		CROSS APPLY (SELECT LeftBracket=CHARINDEX('(', C.CustomerName)) AS LeftBracket
+		CROSS APPLY (SELECT RightBracket=CHARINDEX(')', C.CustomerName, LeftBracket+ 1)) AS RightBracket
+		CROSS APPLY (SELECT SpecName=SUBSTRING(C.CustomerName, LeftBracket+ 1, RightBracket- LeftBracket- 1)) AS CustomerName
+		WHERE C.CustomerID BETWEEN 2 AND 6 
+		)AS Sales1
+PIVOT (SUM(SalesCount) FOR SpecName IN ([Peeples Valley, AZ],[Medicine Lodge, KS],[Gasport, NY], [Sylvanite, MT],[Jessie, ND]))
 as PVT
-ORDER BY [Date];
 
